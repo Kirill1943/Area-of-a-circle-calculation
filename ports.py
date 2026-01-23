e70e41644c4d501073c3d18a5468b54ff3ea95a1
Кирилл <kirillsok2015@gmail.com>
2026-01-23 10:13:45 +0400

    Update ports.py

--- ports.py
+++ ports.py
@@ -1,12 +1,12 @@
 import socket
-from rich import print
+from rich import print as rprint
 
-def main(port_min: int = 0, port_max: int = 1024, ожидание: float = 0.1):
+def main_scan(port_min: int = 0, port_max: int = 1024, timeout: float = 0.1):
     host = '127.0.0.1'
     
     for i in range(port_min, port_max + 1):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-            soc.settimeout(ожидание)
+            soc.settimeout(timeout)
             result = soc.connect_ex((host, i))
 
             protocol = 'неизвестен'
@@ -16,9 +16,18 @@ def main(port_min: int = 0, port_max: int = 1024, ожидание: float = 0.1)
                 pass
 
             if result == 0:
-                print(f'порт {i:5} [#00FF66] ОТКРЫТ[/] (сервис: {protocol})')
+                rprint(f'порт {i:5} [#00FF66] ОТКРЫТ[/] (сервис: {protocol})')
             else:
-                print(f'порт {i:5} [#FF6500] ЗАКРЫТ[/] (сервис: {protocol})')
+                rprint(f'порт {i:5} [#FF6500] ЗАКРЫТ[/] (сервис: {protocol})')
+def main_dns(guery: str = 'google.com'):
+    try:
+        ip = socket.gethostbyname(guery)
+    except Exception as e:
+        print(f'возникла ошибка {e}')
+        ip = None
+
+    rprint(f'запрос: [#00FF66] {guery}, [#00FFDD] ip-адрес: {ip}[/]')
 
 if __name__ == '__main__':
-    main()
+    main_scan(0, 1000, 0.01)
+    main_dns('python.org')
